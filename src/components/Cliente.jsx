6b39c265f9eed8c8cbc9ee6896266193923e2fed
Juan Manuel Parra <39108044+juanco2597@users.noreply.github.com>
2023-01-20 11:04:38 -0500

    correciones

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -12,7 +12,7 @@ function Cliente({ cliente }) {
 
     const { nombre, empresa, email, telefono, id } = cliente
     return (
-        <tr className='border-b w-2'>
+        <tr className='border-b'>
             <td className='p-6 space-y-2'>
                 <p className='text-2xl text-gray-800'>{nombre}</p>
                 <p>{empresa}</p>
