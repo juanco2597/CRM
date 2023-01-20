c71126aafb895340d9705adb400791d15ba6f0af
Juan Manuel Parra <39108044+juanco2597@users.noreply.github.com>
2023-01-20 10:41:40 -0500

    cambio2

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -12,13 +12,13 @@ function Cliente({ cliente }) {
 
     const { nombre, empresa, email, telefono, id } = cliente
     return (
-        <tr className='border-b'>
+        <tr className='border-b w-10'>
             <td className='p-6 space-y-2'>
                 <p className='text-2xl text-gray-800'>{nombre}</p>
                 <p>{empresa}</p>
             </td>
 
-            <td className='p-6'>
+            <td className='p-6 '>
                 <p className='text-gray-600'><span className='text-gray-800 uppercase font-bold'>Email: </span>{email}</p>
                 <p className='text-gray-600'><span className='text-gray-800 uppercase font-bold'>Telefono: </span>{telefono}</p>
             </td>
